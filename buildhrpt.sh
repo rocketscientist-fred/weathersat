da0f81d2680ae8e31a2376d81f29fc670ed5b1c3
rocketscientist-fred <56307512+rocketscientist-fred@users.noreply.github.com>
2019-12-17 15:26:14 +0100

    Add files via upload

---
+++ buildhrpt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $MMODEL = "LARGE" ] 
+then
+  echo "gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double ./src/plotsoft.f"
+  gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double ./src/plotsoft.f
+  echo "gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/sgp4tle.f"
+  gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/sgp4tle.f
+  echo "gfortran -o hrpt.exe -cpp -DLARGE -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/hrpt.f plotsoft.o sgp4tle.o"
+  gfortran -o hrpt.exe -cpp -DLARGE -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/hrpt.f plotsoft.o sgp4tle.o
+  echo "gfortran -o tletrack.exe -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/tletrack.f plotsoft.o sgp4tle.o"
+  gfortran -o tletrack.exe -ffixed-line-length-350 -fno-range-check -mcmodel=large -m64 -malign-double -O2 ./src/tletrack.f plotsoft.o sgp4tle.o
+fi
+if [ $MMODEL = "MEDIUM" ] 
+then
+  echo "gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double ./src/plotsoft.f"
+  gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double ./src/plotsoft.f
+  echo "gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/sgp4tle.f"
+  gfortran -c -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/sgp4tle.f
+  echo "gfortran -o hrpt.exe -cpp -DMEDIUM -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/hrpt.f plotsoft.o sgp4tle.o"
+  gfortran -o hrpt.exe -cpp -DMEDIUM -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/hrpt.f plotsoft.o sgp4tle.o
+  echo "gfortran -o tletrack.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/tletrack.f plotsoft.o sgp4tle.o"
+  gfortran -o tletrack.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/tletrack.f plotsoft.o sgp4tle.o
+fi
+echo "gfortran -o readbin.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/readbin.f"
+gfortran -o readbin.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/readbin.f
+echo "gfortran -o readbin_modis.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/readbin_modis.f"
+gfortran -o readbin_modis.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/readbin_modis.f
+echo "gfortran -o polarstereo.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/polarstereo.f"
+gfortran -o polarstereo.exe -ffixed-line-length-350 -fno-range-check -mcmodel=medium -m64 -malign-double -O2 ./src/polarstereo.f
+rm *.o
